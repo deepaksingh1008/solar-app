31ff210cc311eb15729945fe952aa2bb98e225d0
deepaksingh <ds4850716@gmail.com>
2024-05-27 13:29:41 +0530

    update height

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -55,7 +55,7 @@ const Home = () => {
                 {img.map((item, idx) => (
                     <Box
                         key={idx}
-                        h="80vh"
+                        h="85vh"
                         backgroundImage={`url('${item}')`}
                         backgroundSize="cover"
                         backgroundPosition="center"
